00fe87f6adea854e0413d8eb707019f03ff1bf0c
Daniel Keen <keendaniel01@gmail.com>
2018-05-23 21:07:45 +0100

    chore: install redux-thunk and axios

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -2,12 +2,13 @@ import 'materialize-css/dist/css/materialize.min.css'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import reduxThunk from 'redux-thunk'
 import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/App';
 import reducers from './reducers';
 
-const store = createStore(reducers, {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
   <Provider store={store}>
